2f6091b949cf9f0f0fd152599f7f714fd6c20862
Shax <nedcas69@gmail.com>
2022-05-26 09:02:12 +0500

    mendeleev

--- mendeleev/table.rb
+++ mendeleev/table.rb
@@ -6,8 +6,25 @@ elements = []
 table_hash.each_key { |keys|
     elements << keys
 }
-puts "выберите элемент/n" + elements.to_s
-user_input = STDIN.gets.chomp
-table_hash[user_input].each {|keys, values|
-    puts keys +' -- '+ values.to_s
-}
+
+puts "У нас всего элементов: #{elements.size} "
+puts "\n" + elements.to_s
+
+# Спросим пользователя, о каком элементе он хочет узнать
+puts "\nО каком элементе хотите узнать?"
+element_key = STDIN.gets.chomp
+
+# table_hash[element_key].each {|keys, values|
+#     puts keys +' -- '+ values.to_s
+# }
+
+# Если такой элемент есть в нашем хэше, покажем информацию о нём
+if table_hash.has_key?(element_key)
+    element = table_hash[element_key]
+    puts "Порядковый номер: #{element['number']}"
+    puts "Название: #{element['name']}"
+    puts "Первооткрыватель: #{element['discoverer']} (#{element['year']} г.)"
+    puts "Плотность: #{element['density']} г/см³"
+  else
+    puts 'Извините, про такой элемент мы еще не знаем.'
+  end
